c7a4381ddfe0e41278b25971ba54965582993981
Ju Dantas <jdantas@thoughtworks.com>
2019-01-04 14:42:49 -0200

    Criado miniserviço para rederizar os html no heroku

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .get('/:file', (req, res) => {
+    const { file } = req.params
+    res.sendFile(file, {
+      root: 'views'
+    })
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
